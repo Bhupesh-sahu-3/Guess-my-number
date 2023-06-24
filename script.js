ecb419c15808f72272acbd4204140c8391324fca
Bhupesh Sahu <bhupesh.rv3@gmail.com>
2023-06-24 17:53:52 +0530

    added displayMessage() function

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
@@ -12,12 +16,14 @@ document.querySelector('.check').addEventListener('click', function () {
 
   //when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    //document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!');
   }
 
   //when player wins
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '✅ Correct Number!';
+    // document.querySelector('.message').textContent = '✅ Correct Number!';
+    displayMessage('✅ Correct Number');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -32,12 +38,15 @@ document.querySelector('.check').addEventListener('click', function () {
   //when the guess is wrong
   else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? '📈 Too high!' : '📉 Too low';
+      //   document.querySelector('.message').textContent =
+      //     guess > secretNumber ? '📈 Too high!' : '📉 Too low';
+
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '👎 GAME OVER!!';
+      //   document.querySelector('.message').textContent = '👎 GAME OVER!!';
+      displayMessage('👎 GAME OVER!!');
       document.querySelector('.score').textContent = 0;
     }
   }
