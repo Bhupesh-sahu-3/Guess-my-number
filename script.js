abc9f50e3afe2d2dcef8428e231714c3d8565f7b
Bhupesh Sahu <bhupesh.rv3@gmail.com>
2023-06-24 16:00:50 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 'use strict';
-console.log(document.querySelector('.message').textContent); //selecting element
+// console.log(document.querySelector('.message').textContent); //selecting element
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value = 23;
+let score = 20;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '✅ Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score === 0) {
+      document.querySelector('.message').textContent = '👎 GAME OVER!!';
+    }
+  } else {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score === 0) {
+      document.querySelector('.message').textContent = '👎 GAME OVER!!';
+    }
+  }
+});
