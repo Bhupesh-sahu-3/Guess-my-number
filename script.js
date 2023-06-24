68e1f018d18af11eb60b67d4d1ee7bd51fb0941c
Bhupesh Sahu <bhupesh.rv3@gmail.com>
2023-06-24 16:58:08 +0530

    modified css sytle using DOM manipulation

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent); //selecting element
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
 let score = 20;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +17,9 @@ document.querySelector('.check').addEventListener('click', function () {
   //when player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✅ Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   }
 
   //when the guess is too high
@@ -49,4 +45,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
