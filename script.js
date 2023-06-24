68c3cc60d2701459d2923b2ad2e14e49d91e5715
Bhupesh Sahu <bhupesh.rv3@gmail.com>
2023-06-24 16:12:02 +0530

    modified script.js

--- script.js
+++ script.js
@@ -21,18 +21,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✅ Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
-    if (score === 0) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
       document.querySelector('.message').textContent = '👎 GAME OVER!!';
+      document.querySelector('.score').textContent = 0;
     }
   } else {
-    document.querySelector('.message').textContent = '📉 Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
-    if (score === 0) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
       document.querySelector('.message').textContent = '👎 GAME OVER!!';
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
